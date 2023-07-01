0fdbd3234cbafb00e61a8072f3e6da0f0a002de4
amralgendi <amrag201@gmail.com>
2023-07-01 23:00:53 +0300

    test commit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    DOMAIN: "masjid.net",
-    API_BASE: "https://api.masjid.net/saas",
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
